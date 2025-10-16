eb5ed4d0d2b346d482b3f4447d1fb88b3b156cf6
zDefcon <antholozadarequelme@gmail.com>
2025-10-16 08:01:42 -0500

    feat: Implementa la lógica de la clase Auto

--- src/GUI/Auto.java
+++ src/GUI/Auto.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Vehiculo;
+package GUI;
 
 /**
  *
@@ -13,5 +13,79 @@ public class Auto extends Vehiculo{
     private String tipoCombustible;
     private boolean aireAcondicionado;
     private double descuento;
+    
+    public Auto(){
+        
+    }
+
+    public Auto(int puertas, String tipoCombustible, boolean aireAcondicionado, double descuento) {
+        this.puertas = puertas;
+        this.tipoCombustible = tipoCombustible;
+        this.aireAcondicionado = aireAcondicionado;
+        this.descuento = descuento;
+    }
+
+    public Auto(int puertas, String tipoCombustible, boolean aireAcondicionado, double descuento, String marca, String modelo, int anio, double precioBase) {
+        super(marca, modelo, anio, precioBase);
+        this.puertas = puertas;
+        this.tipoCombustible = tipoCombustible;
+        this.aireAcondicionado = aireAcondicionado;
+        this.descuento = descuento;
+    }
+
+    public int getPuertas() {
+        return puertas;
+    }
+
+    public void setPuertas(int puertas) {
+        if(puertas < 0){
+            this.puertas = 0;
+        }
+        else{
+        this.puertas = puertas;
+        }
+    }
+
+    public String getTipoCombustible() {
+        return tipoCombustible;
+    }
+
+    public void setTipoCombustible(String tipoCombustible) {
+        this.tipoCombustible = tipoCombustible;
+    }
+
+    public boolean isAireAcondicionado() {
+        return aireAcondicionado;
+    }
+
+    public void setAireAcondicionado(boolean aireAcondicionado) {
+        this.aireAcondicionado = aireAcondicionado;
+    }
+
+    public double getDescuento() {
+        return descuento;
+    }
+
+    public void setDescuento(double descuento) {
+        this.descuento = getPrecioBase() * descuento / 100;
+    }
+    
+    @Override
+    public double calcularTotal(){
+        double total = super.calcularTotal();
+        if(isAireAcondicionado()){
+            return total = (total + 800) - getDescuento();
+        }
+        
+        return total = total - getDescuento();
+        
+    }
+    @Override
+    public String mostrarInfo(){
+        String info = super.mostrarInfo();
+        return info + "\nPuertas: " + puertas + "\nTipo Combustible: " + tipoCombustible + "\nAire Acondicionado: " + (isAireAcondicionado() ? "Si" : "No") + "\nDescuento: " + descuento + "\nTotal: " + calcularTotal() + "\n";
+    }
+    
+    
     
 }
