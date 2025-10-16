7d580e10d4e8ee92a2e163dee8fc901fe18222d6
zDefcon <antholozadarequelme@gmail.com>
2025-10-16 01:09:15 -0500

    Initial commit

---
+++ src/Vehiculo/Vehiculo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Vehiculo;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Vehiculo {
+    private String marca;
+    private String modelo;
+    private int anio;
+    private double precioBase;
+    
+    
+}
