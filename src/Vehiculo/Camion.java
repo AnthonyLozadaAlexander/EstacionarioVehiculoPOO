0af6acb207e9f5932b232aa906a87c5a36af3c65
zDefcon <antholozadarequelme@gmail.com>
2025-10-16 06:46:58 -0500

    feat: Implementación inicial de la clase Camion

--- src/Vehiculo/Camion.java
+++ src/Vehiculo/Camion.java
@@ -8,6 +8,65 @@ package Vehiculo;
  *
  * @author USUARIO
  */
-public class Camion {
+public class Camion extends Vehiculo {
+    private int capacidadToneladas;
+    private int ejes;
+    private double costoMantenimiento;
+    private boolean remolque;
+    
+    public Camion(){
+        
+    }
+
+    public Camion(int capacidadToneladas, int ejes, double costoMantenimiento, boolean remolque) {
+        this.capacidadToneladas = capacidadToneladas;
+        this.ejes = ejes;
+        this.costoMantenimiento = costoMantenimiento;
+        this.remolque = remolque;
+    }
+
+    public Camion(int capacidadToneladas, int ejes, double costoMantenimiento, boolean remolque, String marca, String modelo, int anio, double precioBase) {
+        super(marca, modelo, anio, precioBase);
+        this.capacidadToneladas = capacidadToneladas;
+        this.ejes = ejes;
+        this.costoMantenimiento = costoMantenimiento;
+        this.remolque = remolque;
+    }
+
+    
+    
+    public int getCapacidadToneladas() {
+        return capacidadToneladas;
+    }
+
+    public void setCapacidadToneladas(int capacidadToneladas) {
+        this.capacidadToneladas = capacidadToneladas;
+    }
+
+    public int getEjes() {
+        return ejes;
+    }
+
+    public void setEjes(int ejes) {
+        this.ejes = ejes;
+    }
+
+    public double getCostoMantenimiento() {
+        return costoMantenimiento;
+    }
+
+    public void setCostoMantenimiento(double costoMantenimiento) {
+        this.costoMantenimiento = costoMantenimiento;
+    }
+
+    public boolean isRemolque() {
+        return remolque;
+    }
+
+    public void setRemolque(boolean remolque) {
+        this.remolque = remolque;
+    }
+    
+    
     
 }
